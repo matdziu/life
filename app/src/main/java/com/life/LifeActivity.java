8167c9cd57a4349214cfc218803aedb8f6000a4a
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-02-08 09:31:09 +0100

    Added gridLayout and initialised it

--- app/src/main/java/com/life/LifeActivity.java
+++ app/src/main/java/com/life/LifeActivity.java
@@ -3,12 +3,25 @@ package com.life;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.GridLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LifeActivity extends AppCompatActivity {
+
+    private static final int GRID_SPAN = 100;
+
+    @BindView(R.id.grid_layout)
+    GridLayout gridLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_life);
+        ButterKnife.bind(this);
+
+        gridLayout.setRowCount(GRID_SPAN);
+        gridLayout.setColumnCount(GRID_SPAN);
     }
 }
